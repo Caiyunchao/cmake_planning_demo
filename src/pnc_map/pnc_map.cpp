8ab0d488b5d3e7d48f69862b85bf5593dcbc6e3a
cycle <552451959@qq.com>
2025-03-26 15:39:57 +0800

    basic mode

---
+++ src/pnc_map/pnc_map.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "PNC_map.h"
+using namespace std;
+void PNC_Map::mapInfo()
+{
+    cout << "this is pnc_map" << endl;
+}
---
+++ src/process/process.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "process.h"
+void Process::planProcess()
+{
+    cout <<"this is planProcess"<<endl;
+    my_map.mapInfo();
+    cout<< "planning process!"<<endl;
+}
